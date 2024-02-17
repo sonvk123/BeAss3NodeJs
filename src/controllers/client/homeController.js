c7c09fa0d957a00b650b26783219a814da69998c
sonvk123 <70042474+sonvk123@users.noreply.github.com>
2024-02-17 16:37:22 +0700

    update homeController

--- src/controllers/client/homeController.js
+++ src/controllers/client/homeController.js
@@ -1,7 +1,12 @@
 const ProductModel = require("../../models/productModels");
 
 exports.getHomeProducts = async (req, res) => {
-  const Products = await ProductModel.find();
-  // console.log("Products:", Products);
-  res.send({ message: "gửi dữ liệu thành công", products: Products });
+  try {
+    const Products = await ProductModel.find();
+    
+    res.send({ message: "Gửi dữ liệu thành công", products: Products });
+  } catch (error) {
+
+    res.status(500).send({ message: "Lỗi server khi lấy dữ liệu sản phẩm" });
+  }
 };
